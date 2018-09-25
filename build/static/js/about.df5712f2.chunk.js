1223d89cd920b169f46a1c7892ae4380f605cb6a
Endy Santoso <ndxsaber@gmail.com>
2018-09-25 14:28:34 +0700

    initialize ulang

---
+++ build/static/js/about.df5712f2.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{132:function(e,t,a){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l=a(0),u=a.n(l),n=a(44);t.default=function(){return u.a.createElement(n.a,{id:"about",title:"About",description:"This is about really cool stuff."},u.a.createElement("p",null,"What we're all about"))}}});
+//# sourceMappingURL=about.df5712f2.chunk.js.map
